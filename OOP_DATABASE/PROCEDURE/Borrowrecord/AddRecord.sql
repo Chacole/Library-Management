abf4a74fd1c4e4bba8d420bb0fc15484e6f7a794
AnhTu <anhtui9725@gmail.com>
2024-11-19 11:49:19 +0700

    Delete OOP_DATABASE directory

--- OOP_DATABASE/PROCEDURE/Borrowrecord/AddRecord.sql
+++
@@ -1,8 +0,0 @@
-DROP PROCEDURE IF EXISTS AddRecord;
-DELIMITER //
-CREATE PROCEDURE AddRecord(IN UserNameK VARCHAR(255), IN DocumentTitleK VARCHAR(255), IN BorrowDateK DATE)
-BEGIN
-    INSERT INTO borrowrecord (username, documenttitle, borrowdate)
-    VALUES (UserNameK, DocumentTitleK, BorrowDateK);
-END //
-DELIMITER ;
--- OOP_DATABASE/PROCEDURE/Document/AddDocument.sql
+++
@@ -1,8 +0,0 @@
-DROP PROCEDURE IF EXISTS AddDocument;
-DELIMITER //
-CREATE PROCEDURE AddDocument(IN TitleK VARCHAR(255), IN ISBNK VARCHAR(45), IN TypeK VARCHAR(100), IN AuthorK VARCHAR(255), IN PublisherK VARCHAR(255), IN YearPublishedK INT, IN QuantityK INT)
-BEGIN
-    INSERT INTO documents (Title, ISBN, Type, Author, Publisher, YearPublished, Quantity)
-    VALUES (TitleK, ISBNK, TypeK, AuthorK, PublisherK, YearPublishedK, QuantityK);
-END //
-DELIMITER ;

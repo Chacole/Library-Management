f08767d6cfd805484026c586a7becf30a5dce014
AnhTu <anhtui9725@gmail.com>
2024-11-27 22:07:06 +0700

    Delete OOP_DATABASE directory

--- OOP_DATABASE/Fix bug foreignkey/fk users-accounts.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE accounts DROP Foreign Key accounts_ibfk_1; 
-ALTER TABLE accounts ADD constraint accounts_ibfk_1 foreign key (AccountID) references users(ID) ON UPDATE CASCADE ON DELETE CASCADE;
